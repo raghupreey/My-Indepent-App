fe53f56c8cf255f2f0512599a8172cad27700686
raghupreey <89856655+raghupreey@users.noreply.github.com>
2021-12-12 16:26:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function father(){
+    user_name= document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location="Raghawendra.html";
+}
+
+function mother(){
+    user_name= document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location="Priyanka.html";
+}
+
+function brother(){
+    user_name= document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location="Aditya.html";
+}
